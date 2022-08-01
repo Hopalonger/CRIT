7a3a6592ac6ab26a5acc45495343c957c7b43f9c
Ryan Hopkins <Hopalonghacksaw@gmail.com>
2022-08-01 10:46:28 -0700

    Added in New Render Method Along with basic error handling

--- Application.py
+++ Application.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
+from Core.DB import *
+from Pages.index import *
 
 UPLOAD_FOLDER = '.\\uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'csv', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -14,30 +16,26 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    error = ""
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            error = "No File Part"
+            #return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            error = "No Selected File"
+            #return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('download_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        else:
+            error = "File is not in supported file type"
+
+    return GetIndex(error)
 
 @app.route('/uploads/<name>')
 def download_file(name):
